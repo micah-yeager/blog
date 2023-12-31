d3dc72f3475ef97c21e3082556d8183a7b584604
Micah Yeager <micah@yeagers.co>
2023-12-30 23:05:34 -0500

    fix: namespace card group classes

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -17,7 +17,10 @@ export function Card<T extends ElementType = "div">({
 
   return (
     <Component
-      className={clsx(className, "group relative flex flex-col items-start")}
+      className={clsx(
+        className,
+        "group/Card relative flex flex-col items-start",
+      )}
     >
       {children}
     </Component>
@@ -30,7 +33,7 @@ Card.Link = function CardLink({
 }: ComponentPropsWithoutRef<typeof Link>) {
   return (
     <>
-      <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl" />
+      <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover/Card:scale-100 group-hover/Card:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl" />
       <Link {...props}>
         <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl" />
         <span className="relative z-10">{children}</span>
