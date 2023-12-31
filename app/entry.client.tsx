4891d886a9c057d7fc9a756ada99ac9496308381
Micah Yeager <micah@yeagers.co>
2023-12-30 21:29:31 -0500

    feat(perf): cache site static files on site load

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,6 +1,9 @@
 import { RemixBrowser } from "@remix-run/react"
 import { StrictMode, startTransition } from "react"
 import { hydrateRoot } from "react-dom/client"
+import { cacheAssets } from "remix-utils/cache-assets"
+
+cacheAssets().catch(console.error)
 
 startTransition(() => {
   hydrateRoot(
