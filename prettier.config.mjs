6858177aa3780b0f05a39b0ceb5835db65c60d83
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2024-07-09 10:42:32 -0400

    style: de-dupe configurations using JSON imports

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,3 +1,6 @@
+import packageJson from "./package.json" with { type: "json" }
+import tsconfigJson from "./tsconfig.json" with { type: "json" }
+
 /** @type {import("prettier").Config} */
 const baseConfig = {
   plugins: [
@@ -12,8 +15,24 @@ const baseConfig = {
   bracketSpacing: true
 }
 
-const tsconfigPaths = ["@ui", "@content", "~"]
 /** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
+/**
+ * A regex pattern that matches any of the path aliases from `tsconfig.json`.
+ *
+ * Note that this is a string, not RegExp, since the plugin requires strings.
+ *
+ * @type {string}
+ */
+const tsconfigPathAliasesRegex =
+  // Start matching group.
+  "(" +
+  Object.keys(tsconfigJson.compilerOptions.paths)
+    // Remove the trailing "/*" from the path aliases.
+    .map((item) => item.slice(0, item.length - 2))
+    // Add the "or" operator between each path alias
+    .join("|") +
+  // End matching group.
+  ")"
 const sortImportsConfig = {
   importOrder: [
     "", // Empty line for top-of-file comments.
@@ -23,13 +42,21 @@ const sortImportsConfig = {
     "<TYPES>", // Types not matched by other special words or groups.
     "<THIRD_PARTY_MODULES>", // Imports not matched by other special words or groups.
     "",
-    `<TYPES>^(${tsconfigPaths.join("|")})(/.*)$`, // Internal types.
-    `^(${tsconfigPaths.join("|")})(/.*)$`, // Internal imports.
+    `<TYPES>^${tsconfigPathAliasesRegex}(/.*)$`, // Internal types.
+    `^${tsconfigPathAliasesRegex}(/.*)$`, // Internal imports.
     "",
     "<TYPES>^[.]", // Relative types.
     "^[.]" // Relative imports.
   ],
-  importOrderTypeScriptVersion: "5.1.6" // TypeScript version used by project.
+  // Add the "importAttributes" Babel plugin, in addition to the defaults.
+  importOrderParserPlugins: [
+    "typescript",
+    "jsx",
+    "importAttributes",
+    "decorators"
+  ],
+  // Remove the leading version indicator from the TypeScript package version.
+  importOrderTypeScriptVersion: packageJson.devDependencies.typescript.slice(1)
 }
 
 /** @type {import("prettier-plugin-jsdoc").Options} */
