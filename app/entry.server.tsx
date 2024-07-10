bb2a1e84c184ad002dbfd31eb59aae13962d3891
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2024-07-10 12:27:38 -0400

    refactor: update `entry.server` to use Vercel base

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,61 +1,18 @@
-import { PassThrough } from "node:stream"
-
-import type { AppLoadContext, EntryContext } from "@remix-run/node"
-import { createReadableStreamFromReadable } from "@remix-run/node"
+import type { EntryContext } from "@vercel/remix"
 import { RemixServer } from "@remix-run/react"
-import { renderToPipeableStream } from "react-dom/server"
-import { preloadRouteAssets } from "remix-utils/preload-route-assets"
+import { handleRequest } from "@vercel/remix"
 
-const ABORT_DELAY = 5_000
-
-export default function handleRequest(
+export default async function (
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext,
-  _loadContext: AppLoadContext
+  remixContext: EntryContext
 ) {
-  return new Promise((resolve, reject) => {
-    let shellRendered = false
-    const { pipe, abort } = renderToPipeableStream(
-      <RemixServer
-        context={remixContext}
-        url={request.url}
-        abortDelay={ABORT_DELAY}
-      />,
-      {
-        onShellReady() {
-          shellRendered = true
-          const body = new PassThrough()
-          const stream = createReadableStreamFromReadable(body)
-
-          responseHeaders.set("Content-Type", "text/html")
-          preloadRouteAssets(remixContext, responseHeaders)
-
-          resolve(
-            new Response(stream, {
-              headers: responseHeaders,
-              status: responseStatusCode
-            })
-          )
-
-          pipe(body)
-        },
-        onShellError(error: unknown) {
-          reject(error)
-        },
-        onError(error: unknown) {
-          responseStatusCode = 500
-          // Log streaming rendering errors from inside the shell.  Don't log
-          // errors encountered during initial shell rendering since they'll
-          // reject and get logged in handleDocumentRequest.
-          if (shellRendered) {
-            console.error(error)
-          }
-        }
-      }
-    )
-
-    setTimeout(abort, ABORT_DELAY)
-  })
+  const remixServer = <RemixServer context={remixContext} url={request.url} />
+  return handleRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    remixServer
+  )
 }
