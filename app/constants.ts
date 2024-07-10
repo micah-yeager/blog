0ac0f7f696226206249679a4cfcee245095c269a
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2024-07-10 06:48:33 -0400

    fix: add missing `LOCALE` constant

--- app/constants.ts
+++ app/constants.ts
@@ -4,3 +4,5 @@ export const FULL_NAME = `${FIRST_NAME} ${LAST_NAME}`
 
 export const GITHUB_URL = "https://github.com/micah-yeager"
 export const LINKEDIN_URL = "https://www.linkedin.com/in/micahyeager"
+
+export const LOCALE = "en-US"
