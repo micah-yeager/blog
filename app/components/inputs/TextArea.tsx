29731ce4d2056fce58d7ddf8f8c4638e491b69fc
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2024-07-12 11:15:13 -0400

    feat: vertically auto-expand `textarea`s on user input

--- app/components/inputs/TextArea.tsx
+++ app/components/inputs/TextArea.tsx
@@ -1,10 +1,12 @@
-import type { ComponentPropsWithoutRef } from "react"
+import type { ComponentPropsWithoutRef, FormEvent } from "react"
 import { ExclamationCircleIcon } from "@heroicons/react/24/solid"
 import clsx from "clsx"
-import { forwardRef } from "react"
+import { forwardRef, useState } from "react"
 
 import { Icon } from "@ui/Icon"
 import { InlineAddOn } from "@ui/inputs/shared/InlineAddOn"
+import { useForwardRef } from "@utils/hooks"
+import { tw } from "@utils/templates"
 
 import type { SharedFormProps } from "./shared"
 import { InputDescription } from "./shared/InputDescription"
@@ -26,13 +28,33 @@ export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
       hideOptionalHint,
       disabled,
       required,
+      onInput: inputOnInput,
       className,
       ...rest
     },
     forwardedRef
   ) {
+    const ref = useForwardRef(forwardedRef)
+
     // shortcut for showing the `optional` hint
     const showOptionalHint = !hideOptionalHint && !required
+
+    // Rendering control for auto-resizing.
+    const [value, setValue] = useState<string>(ref.current?.value ?? "")
+    const onInput = (event: FormEvent<HTMLTextAreaElement>) => {
+      setValue(event.currentTarget.value)
+      // Call the provided `onInput` if applicable.
+      inputOnInput?.(event)
+    }
+
+    const gridClassName = tw`col-start-1 col-end-2 row-start-1 row-end-2`
+    // Define classes here since they will be used for both the textarea and the
+    // invisible "preview" used for auto-resizing.
+    const baseClassName = clsx(
+      gridClassName,
+      "block min-h-[2.25rem] w-full rounded-md border-0 px-3 py-1.5 text-inherit text-zinc-900 shadow-sm ring-1 ring-inset ring-zinc-300 placeholder:text-zinc-400 focus:ring-2 focus:ring-inset focus:ring-primary-500 disabled:cursor-not-allowed disabled:bg-zinc-100 disabled:text-zinc-500 disabled:ring-zinc-200 dark:bg-black dark:text-zinc-100 dark:ring-zinc-700 focus:dark:ring-primary-500 dark:disabled:bg-zinc-900 dark:disabled:ring-zinc-800 sm:text-sm sm:leading-6",
+      error ? "pr-10 ring-red-500" : "ring-zinc-300 dark:ring-zinc-700"
+    )
 
     return (
       <div className={clsx("space-y-2", className)}>
@@ -58,18 +80,24 @@ export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
           )}
 
           <div className="relative">
-            <textarea
-              {...rest}
-              {...{ required, disabled }}
-              ref={forwardedRef}
-              className={clsx(
-                "block min-h-[2.25rem] w-full rounded-md border-0 py-1.5 text-zinc-900 shadow-sm ring-1 ring-inset ring-zinc-300 placeholder:text-zinc-400 focus:ring-2 focus:ring-inset focus:ring-primary-500 disabled:cursor-not-allowed disabled:bg-zinc-100 disabled:text-zinc-500 disabled:ring-zinc-200 dark:bg-black dark:text-zinc-100 dark:ring-zinc-700 focus:dark:ring-primary-500 dark:disabled:bg-zinc-900 dark:disabled:ring-zinc-800 sm:text-sm sm:leading-6",
-                error
-                  ? "pr-10 ring-red-500"
-                  : "ring-zinc-300 dark:ring-zinc-700"
-              )}
-              aria-invalid={Boolean(error)}
-            />
+            <div className="grid">
+              <textarea
+                {...rest}
+                {...{ required, disabled, onInput, ref }}
+                // Disable manual resizing, since it breaks auto-resizing.
+                className={clsx(baseClassName, "resize-none overflow-clip")}
+                aria-invalid={Boolean(error)}
+              />
+              <div
+                // Mimic `textarea`'s `whitespace-pre-wrap` to ensure the
+                // preview behavior matches the textarea.
+                className={clsx(baseClassName, "invisible whitespace-pre-wrap")}
+                aria-hidden
+              >
+                {/* The trailing space ensures trailing newlines without content still force vertical expansion. */}
+                {value}{" "}
+              </div>
+            </div>
 
             {/* trailing inline add-on */}
             {error && (
