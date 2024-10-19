2a5a2f4b4e2cb33c0cb491863f33ddb2e8d700f6
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2024-10-19 10:38:07 -0400

    test(ui): add `Badge` test

---
+++ app/components/Badge/Badge.test.tsx
@@ -0,0 +1,61 @@
+import { cleanup, render, screen, waitFor } from "@testing-library/react"
+import { afterEach, test } from "vitest"
+
+import { createRemixStub } from "@remix-run/testing"
+import type { ComponentPropsWithoutRef } from "react"
+import { Badge, BadgeButton } from "./Badge"
+
+afterEach(cleanup)
+
+test.each([
+  { testName: "empty" },
+  {
+    testName: "contentful",
+    children: "content",
+  },
+] satisfies (ComponentPropsWithoutRef<typeof Badge> & { testName: string })[])(
+  "$testName Badge",
+  async ({ testName: _, ...props }) => {
+    const RemixStub = createRemixStub([
+      {
+        path: "/",
+        Component() {
+          return <Badge {...props} />
+        },
+      },
+    ])
+
+    render(<RemixStub />)
+
+    if (props.children) {
+      await waitFor(() => screen.findByText(props.children))
+    }
+  },
+)
+
+test.each([
+  {
+    testName: "button",
+    children: "content",
+  },
+  {
+    testName: "link",
+    children: "content",
+    to: "#",
+  },
+] satisfies (ComponentPropsWithoutRef<typeof BadgeButton> & {
+  testName: string
+})[])("$testName BadgeButton", async ({ testName: _, ...props }) => {
+  const RemixStub = createRemixStub([
+    {
+      path: "/",
+      Component() {
+        return <BadgeButton {...props} />
+      },
+    },
+  ])
+
+  render(<RemixStub />)
+
+  await waitFor(() => screen.findByText(props.children))
+})
