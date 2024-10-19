03d9ef5573ca70cd772119ccab9c92429215192e
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2024-10-19 10:37:55 -0400

    test(ui): add `Alert` test

---
+++ app/components/Alert/Alert.test.tsx
@@ -0,0 +1,40 @@
+import { cleanup, screen, waitFor } from "@testing-library/react"
+import { render } from "@testing-library/react"
+import { afterEach, test } from "vitest"
+
+import type { ComponentPropsWithoutRef } from "react"
+import { Alert } from "./Alert"
+
+afterEach(cleanup)
+
+test.each([
+  {
+    testName: "empty",
+    variant: "info",
+  },
+  {
+    testName: "contentful",
+    variant: "info",
+    children: "content",
+  },
+  {
+    testName: "empty titled",
+    variant: "info",
+    title: "title",
+  },
+  {
+    testName: "contentful titled",
+    variant: "info",
+    title: "title",
+    children: "content",
+  },
+] satisfies (ComponentPropsWithoutRef<typeof Alert> & { testName: string })[])(
+  "$testName Badge",
+  async ({ testName: _, ...props }) => {
+    render(<Alert {...props} />)
+
+    if (props.children) {
+      await waitFor(() => screen.findByText(props.children))
+    }
+  },
+)
