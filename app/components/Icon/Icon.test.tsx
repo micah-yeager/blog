f65783f29d25bdf3637b517cf277a2d147305d2c
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2024-10-20 16:13:22 -0400

    test(ui): add `Icon` test

---
+++ app/components/Icon/Icon.test.tsx
@@ -0,0 +1,21 @@
+import { faCheck } from "@fortawesome/free-solid-svg-icons"
+import { CheckIcon } from "@heroicons/react/24/solid"
+import { cleanup, render, screen } from "@testing-library/react"
+import { afterEach, expect, test } from "vitest"
+import type { FCCase } from "../../../test/types"
+import { Icon } from "./Icon"
+
+afterEach(cleanup)
+
+test.each([
+  { testName: "Font Awesome", as: faCheck },
+  { testName: "Heroicons", as: CheckIcon },
+] satisfies FCCase<typeof Icon>[])(
+  "$testName Icon",
+  async ({ testName, ...props }) => {
+    render(<Icon {...props} data-testid={testName} />)
+
+    const element = screen.getByTestId(testName)
+    expect(element.tagName).toEqual("svg")
+  },
+)
