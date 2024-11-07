174058ae3b5b7cbe41ec83abec2982717cd0577a
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2024-11-07 07:25:16 -0500

    refactor(ui): move single-use `PostLayout` component to route

--- app/routes/posts.$id/route.tsx
+++ app/routes/posts.$id/route.tsx
@@ -6,9 +6,15 @@ import { useMemo } from "react"
 
 import type { Post } from "@services/posts.server"
 import { getPost } from "@services/posts.server"
-import { PostLayout } from "@ui/PostLayout"
 import { mergeMeta } from "@utils/meta"
 
+import { ArrowLeftIcon, ChevronDoubleUpIcon } from "@heroicons/react/24/outline"
+import { Button } from "@ui/Button"
+import { Container } from "@ui/Container"
+import { Icon } from "@ui/Icon"
+import { Prose } from "@ui/Prose"
+import clsx from "clsx"
+import { DateTime } from "luxon"
 import { CANONICAL_ORIGIN, FULL_NAME, LOCALE } from "../../constants"
 import codeStyles from "./prism.css?url"
 
@@ -102,9 +108,94 @@ export default function Route() {
     return getMDXComponent(post.code)
   }, [post.code])
 
+  const created = DateTime.fromISO(post.meta.created)
+  const updated = post.meta.updated ? DateTime.fromISO(post.meta.updated) : null
+
   return (
-    <PostLayout meta={post.meta}>
-      <Component />
-    </PostLayout>
+    <Container className="mt-16 lg:mt-32">
+      <div className="xl:relative">
+        <div className="mx-auto max-w-2xl">
+          {/* back button */}
+          <div className="lg:absolute lg:left-12 lg:-mt-1 lg:mb-0 lg:h-full xl:-top-1.5 xl:left-0 xl:mt-0">
+            <div className="gap-4 space-y-2 lg:sticky lg:top-24">
+              <Button
+                to="/posts"
+                prefetch="intent"
+                color="dark"
+                className="mb-8 inline-block lg:mb-0"
+                aria-label="Go back to all posts"
+              >
+                <Icon
+                  as={ArrowLeftIcon}
+                  className="size-4"
+                  aria-hidden="true"
+                />
+              </Button>
+              <ScrollToTop className="hidden lg:block" />
+            </div>
+          </div>
+
+          {/* content */}
+          <article>
+            <header className="flex flex-col">
+              {/* title */}
+              <h1 className="mt-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+                {post.meta.title}
+              </h1>
+
+              {/* meta */}
+              <time
+                dateTime={post.meta.created}
+                className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"
+              >
+                <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
+                <span className="ml-3">
+                  {created.toLocaleString(DateTime.DATE_FULL)}
+                </span>
+              </time>
+              {updated && (
+                <>
+                  <span className="mx-3">•</span>
+                  <time
+                    dateTime={post.meta.updated}
+                    className="order-last flex items-center text-base text-zinc-400 dark:text-zinc-500"
+                  >
+                    <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
+                    <span className="ml-3">
+                      {updated.toLocaleString(DateTime.DATE_FULL)}
+                    </span>
+                  </time>
+                </>
+              )}
+            </header>
+
+            {/* body */}
+            <Prose className="mt-8" data-mdx-content>
+              <Component />
+            </Prose>
+          </article>
+        </div>
+        <ScrollToTop className="sticky bottom-4 ml-auto mt-8 block lg:hidden" />
+      </div>
+    </Container>
+  )
+}
+
+/**
+ * A floating button that scrolls to the top of the page
+ * when clicked.
+ *
+ * @param className - Additional classes to apply to the button.
+ */
+function ScrollToTop({ className }: { className?: string }) {
+  return (
+    <Button
+      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      color="dark"
+      className={clsx("lg:relative lg:mb-0", className)}
+      aria-label="Scroll to top"
+    >
+      <Icon as={ChevronDoubleUpIcon} className="size-4" aria-hidden="true" />
+    </Button>
   )
 }
