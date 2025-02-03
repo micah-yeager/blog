1c250a07fa79dc57cbd706c7063f12d1c4c6dcae
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2025-02-03 13:33:00 -0500

    refactor: remove Vercel Remix config preset

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,6 @@
 import type { Config } from "@react-router/dev/config"
-import { vercelPreset } from "@vercel/remix/vite"
 
 export default {
-  presets: [vercelPreset()],
   future: {
     unstable_optimizeDeps: true,
   },
