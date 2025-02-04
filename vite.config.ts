1499ffc2a8e4022cff4d68c127190f3f34ffd195
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2025-02-04 07:25:19 -0500

    fix(styles): replace TailwindCSS Vite plugin with PostCSS for now

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 /// <reference types="vitest" />
 
 import { reactRouter } from "@react-router/dev/vite"
-import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig(({ isSsrBuild, command }) => ({
-  plugins: [
-    tailwindcss(),
-    !process.env.VITEST && reactRouter(),
-    tsconfigPaths(),
-  ],
+  plugins: [!process.env.VITEST && reactRouter(), tsconfigPaths()],
   build: {
     rollupOptions: isSsrBuild
       ? {
