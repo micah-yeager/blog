907259a27772f6a3be8e24f07687389902549668
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2025-02-14 15:48:11 -0500

    chore(deps): add `@vercel/react-router` dependency

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "@react-router/dev/config"
+import { vercelPreset } from "@vercel/react-router/vite"
 
 export default {
+  presets: [vercelPreset()],
   ssr: true,
   future: {
     unstable_optimizeDeps: true,
