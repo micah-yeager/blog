51a6ae721372c7dafd81ee435029509a90223a0f
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2025-02-16 20:18:30 -0500

    Revert "fix(build): remove `esbuild` from server bundle"
    This reverts commit b6629a31286d7883bfcdaa47d621a675f620a81f.

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,22 @@ import { reactRouter } from "@react-router/dev/vite"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
-export default defineConfig(
-  ({ command }) =>
-    ({
-      plugins: [!process.env.VITEST && reactRouter(), tsconfigPaths()],
-      ssr: {
-        noExternal: command === "build" ? true : undefined,
-        external: ["esbuild"],
-      },
-      test: {
-        setupFiles: ["test/setup/extend-expect.ts"],
-        clearMocks: true,
-        environment: "jsdom",
-        coverage: {
-          // Leave disabled by default, will be enabled on-demand by the
-          // test:coverage script.
-          provider: "v8",
-          all: true,
-          reporter: ["json-summary", "json"],
-          reportOnFailure: true,
-        },
-      },
-    }) as const,
-)
+export default defineConfig(({ command }) => ({
+  plugins: [!process.env.VITEST && reactRouter(), tsconfigPaths()],
+  ssr: {
+    noExternal: command === "build" ? true : undefined,
+  },
+  test: {
+    setupFiles: ["test/setup/extend-expect.ts"],
+    clearMocks: true,
+    environment: "jsdom",
+    coverage: {
+      // Leave disabled by default, will be enabled on-demand by the
+      // test:coverage script.
+      provider: "v8",
+      all: true,
+      reporter: ["json-summary", "json"],
+      reportOnFailure: true,
+    },
+  },
+}))
